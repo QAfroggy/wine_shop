7ddd56871cdb6609f8c642d610ca83c7e71ae8f9
Murat <murat.karaiev@yahoo.com>
2026-08-15 22:54:55 +0200

    Create class Wine

---
+++ WineShop/Wine.cs
@@ -0,0 +1,16 @@
+public class Wine
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public int Year { get; set;}
+    public decimal Price { get; set;}
+
+
+
+
+
+
+
+
+
+}
