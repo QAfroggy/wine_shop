e0321913151c31f27d166095dffda9278caba50f
Murat <murat.karaiev@yahoo.com>
2026-08-16 21:38:46 +0200

    Feat: add age verification

--- WineShop/Program.cs
+++ WineShop/Program.cs
@@ -1,5 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hi, what's your name?!");
+/*
+Ticket #WS-001: Customer Age Verification
+User Story:
+As a store owner,
+I want to verify the age of users when they start the application,
+so that I can legally sell alcohol only to adults (18+).
 
-string userName = Console.ReadLine();
-Console.WriteLine($"Hi {userName}, Welcome to the Wine Shop!");
+Acceptance Criteria (AC):
+
+The console must ask the user: "Please enter your age:".
+
+The program must read the user's input.
+
+If the age is less than 18, print: "Sorry, access to the store is allowed only for persons 18 and older." and stop the program.
+
+If the age is 18 or older, print: "Age verified. Welcome to the catalog!" and continue the program.
+
+
+*/
+Console.WriteLine("Welcome to the Wine Shop Online!");
+Console.WriteLine("Please enter your age:");
+string ageInput = Console.ReadLine();
+int age = Convert.ToInt32(ageInput);
+if (age <18)
+{
+    Console.WriteLine("Sorry, access to the store is allowed only for persons 18 and older.");
+    return;
+
+}else
+{
+    Console.WriteLine("Age verified. Welcome to the catalog!");
+}
--- WineShop/obj/Debug/net9.0/WineShop.AssemblyInfo.cs
+++ WineShop/obj/Debug/net9.0/WineShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b8c94227204264586432881d3abdfbec0412bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bbc91d251fb14c5473b425b884640fcb3792c6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
