e3136efcdf5f939c816132ee833069b9862b2e53
Murat <murat.karaiev@yahoo.com>
2026-08-18 19:36:05 +0200

    user commit

--- WineShop/obj/Debug/net9.0/WineShop.AssemblyInfo.cs
+++ WineShop/obj/Debug/net9.0/WineShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a166b419be5fe07e7503981e988e8a70d8c4320")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd9986c1aa23ac11e52711bffa778592a932398c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
